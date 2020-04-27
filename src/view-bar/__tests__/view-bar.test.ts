ebab0e6a54274cc133b04692e0826a22d1e21c9e
kv-work <komaroff4@yandex.ru>
2020-04-27 21:25:32 +0500

    add (view-bar): added methods and properties. Added tests

--- src/view-bar/__tests__/view-bar.test.ts
+++ src/view-bar/__tests__/view-bar.test.ts
@@ -1,4 +1,6 @@
 import ViewBar from '../view-bar';
+import { Observer } from '../view-bar';
+
 
 const $ = require('jquery');
 
@@ -6,21 +8,82 @@ describe('ViewBar', () => {
 
   document.body.innerHTML = `<div id="container"></div>`;
   const testNode = document.getElementById('container');
-  let testBar: ViewBar;
+  let testBar: ViewBar,
+      updateFunc: () => {},
+      anotherUpdateFunc: () => {},
+      observer: Observer,
+      anotherObserver: Observer,
+      entries: Array<any>;
 
   beforeEach( () => {
     testBar = new ViewBar(testNode);
+    updateFunc = jest.fn();
+    anotherUpdateFunc = jest.fn();
+    observer = {
+      update: updateFunc
+    };
+    
+    anotherObserver = {
+      update: anotherUpdateFunc
+    };
+
+    entries = Object.entries(testBar);
   } )
 
   test('constructor should return object', () => {
     expect(testNode).not.toBeUndefined()
-    
-    const entries: Array<any> = Object.entries(testBar);
 
     entries.forEach( (entry: [String, any]) => {
       if (entry[0] === '$container') {
         expect($(testNode)).toEqual(entry[1])
       }
     } )
+  })
+
+  test('constructor should append div.slider__bar to container', () => {
+    expect($('.slider__bar').length).not.toBe(0)
+  })
+
+  test('addObserver should added observer to this.observers', () => {
+    expect(testBar).toHaveProperty('observers');
+    testBar.addObserver(observer);
+
+    entries = Object.entries(testBar)
+    entries.forEach( (entry: [String, any], index: number) => {
+      if (entry[0] === 'observers') {
+        expect(entry[1].has(observer)).toBeTruthy()
+      }
+    });
+  })
+
+  test('removeObserver should removed observer', () => {
+    expect(testBar).toHaveProperty('observers');
+    entries = Object.entries(testBar)
+
+    //Add observer
+    testBar.addObserver(observer);
+    entries.forEach( (entry: [String, any], index: number) => {
+      if (entry[0] === 'observers') {
+        expect(entry[1].has(observer)).toBeTruthy()
+      }
+    });
+
+    //Remove observer
+    testBar.removeObserver(observer);
+    entries.forEach( (entry: [String, any], index: number) => {
+      if (entry[0] === 'observers') {
+        expect(entry[1].has(observer)).toBeFalsy()
+      }
+    });
+  })
+
+  test('attachEventListeners added eventListener to bar, which calls notify func', () => {
+    testBar.addObserver(observer);
+    testBar.addObserver(anotherObserver);
+
+    $('.slider__bar').click();
+
+    expect(updateFunc).toHaveBeenCalled()
+    expect(anotherUpdateFunc).toHaveBeenCalled()
   })
 })
--- src/view-bar/view-bar.ts
+++ src/view-bar/view-bar.ts
@@ -1,9 +1,46 @@
 import $ from 'jquery'
 
+interface Observer {
+  update: (value: number) => {}
+}
+
 export default class ViewBar {
   private $container: JQuery
+  private $bar: JQuery
+  private observers: Set<Observer>
 
   constructor(container: HTMLElement) {
-    this.$container = $(container)
+    this.$container = $(container);
+    this.$bar = $('<div>', {
+      class: 'slider__bar'
+    });
+
+    this.$container.append(this.$bar)
+    this.observers = new  Set();
+
+    this.attachEventListeners();
+  }
+
+  private attachEventListeners() {
+    this.$bar.click( (event) => {
+      //Здесь нужно высчитать какое значение было выбрано
+      this.notify(1);
+    } )
+  }
+
+  public addObserver(observer: Observer): void {
+    this.observers.add(observer)
+  }
+
+  public removeObserver(observer: Observer): void {
+    this.observers.delete(observer)
+  }
+
+  public notify(value: number): void {
+    this.observers.forEach( (observer: Observer): void => {
+      observer.update(value);
+    } )
   }
 }
+
+export { Observer };
