dd63a9e32d827b30f2c2241925eb252f1a625789
kv-work <komaroff4@yandex.ru>
2020-05-01 21:08:26 +0500

    created Presenter class

--- src/presenter/presenter.ts
+++ src/presenter/presenter.ts
@@ -1,1 +1,10 @@
-export default class Presenter {}
+import Model from "../model/model";
+import { Observer, View } from '../types';
+
+
+export default class Presenter {
+  private views: Set<View>
+  private model: Model
+  private viewObserver: Observer
+  private modelObserver: Observer
+}
