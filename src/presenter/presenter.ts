5e4c72138d50bf83da1f56a6ea7799d00a8cfbb9
kv-work <komaroff4@yandex.ru>
2020-05-02 10:30:14 +0500

    add (presenter): added constructor and methods

--- src/presenter/presenter.ts
+++ src/presenter/presenter.ts
@@ -1,5 +1,5 @@
 import Model from "../model/model";
-import { Observer, View } from '../types';
+import { OptionsModel, Observer, ViewData, View } from '../types';
 
 
 export default class Presenter {
@@ -7,4 +7,29 @@ export default class Presenter {
   private model: Model
   private viewObserver: Observer
   private modelObserver: Observer
+
+  constructor(model: Model, ...views: Array<View>) {
+    this.model = model;
+    this.views = new Set(views)
+
+    this.viewObserver = {
+      update: (value: number) => {}
+    }
+
+    this.modelObserver = {
+      update: () => {}
+    }
+  }
+
+  private getModelData(): void {}
+
+  private sentModelObserver(model: Model, observer: Observer): void {}
+
+  private sentViewObserver(view: View, observer: Observer): void {}
+
+  private updateView(view: View, updateData: ViewData): void {}
+
+  public renderView(view: View, renderData: ViewData): void {}
+
+  public updateModel(model: Model, updateData: OptionsModel): void {}
 }
