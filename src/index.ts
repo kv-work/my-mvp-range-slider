09a4a160e464cf18f044e88be3f37427b545be0b
kv-work <komaroff4@yandex.ru>
2020-05-25 15:09:35 +0500

    add (index.ts): added another options for slider. added vertical range slider to layout

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,32 @@ const options: ApplicationOption = {
   displayValue: false,
   displayMin: false,
   displayMax: false,
-  onStart: () => console.log('start'),
-  onChange: () => console.log('change'),
-  onFinish: () => console.log('finish'),
-  onUpdate: () => console.log('update'),
+  onStart: () => {},
+  onChange: () => {},
+  onFinish: () => {},
+  onUpdate: () => {},
+};
+
+const anotherOptions: ApplicationOption = {
+  maxValue: 100,
+  minValue: 0,
+  step: 10,
+  value: 30,
+  secondValue: 75,
+  isHorizontal: false,
+  range: true,
+  dragInterval: false,
+  runner: true,
+  bar: true,
+  scale: false,
+  displayValue: false,
+  displayMin: false,
+  displayMax: false,
+  onStart: () => {},
+  onChange: () => {},
+  onFinish: () => {},
+  onUpdate: () => {},
 };
 
 $('.js-test_with_options').myMVPSlider(options);
+$('.js-test_with_another_options').myMVPSlider(anotherOptions);
