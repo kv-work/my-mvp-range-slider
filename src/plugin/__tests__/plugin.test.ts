7bc43555bc801ae309ce0a3364daa9d5f6509196
kv-work <komaroff4@yandex.ru>
2020-05-28 13:08:07 +0500

    refact (plugin): added declaration. added tests descriptions

---
+++ src/plugin/__tests__/plugin.test.ts
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+describe('myMVPSlider', () =>{
+  document.body.innerHTML = '<div class="js-container"></div>';
+
+  const testNode = document.getElementsByClassName('js-container')[0];
+
+  beforeEach(() => {
+    $(testNode).myMVPSlider();
+  });
+
+  test('should render slider with default options', () => {
+    // expect($('.js-slider__bar').length).toBe(1);
+    // expect($('.js-slider__runner').length).toBe(1);
+    // expect($('.js-slider__scale').length).toBe(1);
+  });
+
+  test('should support call chains');
+  test('should define a namespace for working with the plugin');
+  test('should have some methods');
+  test('should save data in data attributes');
+});
--- src/plugin/index.d.ts
+++ src/plugin/index.d.ts
@@ -1,7 +1,45 @@
-import { ApplicationOption } from '../types';
+// Type definitions for myMVPSlider
+// Definitions by: Komarov Vyacheslav <github.com/kv-work/>
 
 declare global {
   interface JQuery {
     myMVPSlider(options?: ApplicationOption): JQuery;
   }
+}
+
+export interface ApplicationOption {
+  // model
+  maxValue?: number;
+  minValue?: number;
+  step?: number;
+  value?: number;
+  secondValue?: number;
+
+  // view
+  isHorizontal?: boolean;
+  range?: boolean;
+  dragInterval?: boolean;
+  runner?: boolean;
+  bar?: boolean;
+  scale?: boolean;
+  scaleStep?: number;
+  displayScaleValue?: boolean;
+  displayValue?: boolean;
+  displayMin?: boolean;
+  displayMax?: boolean;
+  prefix?: string;
+  postfix?: string;
+
+  // presenter
+  dataValues?: Stringable[];
+
+  // callbacks
+  onStart?: CallableFunction;
+  onChange?: CallableFunction;
+  onFinish?: CallableFunction;
+  onUpdate?: CallableFunction;
+}
+
+export interface Stringable {
+  toString(): string;
 }
