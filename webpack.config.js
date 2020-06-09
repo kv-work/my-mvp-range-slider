2111d2c3e5acdca7b84f2e0f6e2edeb6d289d8a0
kv-work <komaroff4@yandex.ru>
2020-06-09 11:24:10 +0500

    fixed eslint errors in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,48 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const ghpages = require('gh-pages');
 
-ghpages.publish('dist', function(err) {});
+ghpages.publish('dist');
 
 module.exports = (env = {}) => {
+  const { mode = 'development' } = env;
 
-  const {
-    mode = "development"
-  } = env;
+  const isProd = mode === 'production';
 
-  const isProd = mode === "production";
-  const isDev = mode === "development";
-
-  //Функция для настройки loader'ов стилей
+  // Функция для настройки loader'ов стилей
   function getStyleLoaders() {
     return [
       isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-      'css-loader'
-    ]
+      'css-loader',
+    ];
   }
 
-  //Функция для настройки подключаемых plagin'ов
+  // Функция для настройки подключаемых plagin'ов
   function getPlugins() {
     const plugins = [
       new HtmlWebpackPlugin({
         filename: 'index.html',
-        template: './src/demo/index.html'
+        template: './src/demo/index.html',
       }),
 
       new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      })
+        'window.jQuery': 'jquery',
+      }),
     ];
 
     if (isProd) {
       plugins.push(new MiniCssExtractPlugin({
-        filename: '[name].css'
-      }))
+        filename: '[name].css',
+      }));
     }
 
-    return plugins
+    return plugins;
   }
 
-  //Объект настроек
+  // Объект настроек
   return {
 
     mode,
@@ -54,35 +50,34 @@ module.exports = (env = {}) => {
     entry: './src/demo/index.ts',
 
     output: {
-      filename: 'index.js'
+      filename: 'index.js',
     },
 
     resolve: {
-      extensions: [ '.ts', '.js' ]
+      extensions: ['.ts', '.js'],
     },
 
     // devtool: 'inline-source-map',
 
     module: {
-      //Loading TypeScript files
+      // Loading TypeScript files
       rules: [
         {
           test: /\.ts$/,
           use: 'ts-loader',
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
 
-        //Loading CSS
+        // Loading CSS
         {
           test: /\.css$/,
-          use: getStyleLoaders()
+          use: getStyleLoaders(),
         },
-      ]
+      ],
     },
 
-    //Loading plugins
-    plugins: getPlugins()
+    // Loading plugins
+    plugins: getPlugins(),
 
-  }
-
-}
+  };
+};
