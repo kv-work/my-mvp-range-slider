35fc6319c912eb8bf3d4a1a336abfcdea5ec2495
kv-work <komaroff4@yandex.ru>
2020-07-18 13:43:21 +0500

    used IIFE

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,50 +1,47 @@
 /* eslint-disable no-param-reassign */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable no-shadow */
-/* eslint-disable func-names */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable eol-last */
 import SliderApp from '../app/app';
 
-$.fn.myMVPSlider = function (options: App.Option): JQuery {
-  const settings = $.extend({
-    maxValue: 100,
-    minValue: 0,
-    step: 1,
-    value: 0,
-    secondValue: undefined,
+(function ($: JQueryStatic): void {
+  $.fn.myMVPSlider = function (options: App.Option): JQuery {
+    const settings = $.extend({
+      maxValue: 100,
+      minValue: 0,
+      step: 1,
+      value: 0,
+      secondValue: undefined,
 
-    // view
-    isHorizontal: true,
-    range: false,
-    dragInterval: false,
-    runner: true,
-    bar: true,
-    scale: false,
-    scaleStep: 1,
-    displayScaleValue: false,
-    displayValue: false,
-    displayMin: false,
-    displayMax: false,
-    prefix: '',
-    postfix: '',
+      // view
+      isHorizontal: true,
+      range: false,
+      dragInterval: false,
+      runner: true,
+      bar: true,
+      scale: false,
+      scaleStep: 1,
+      displayScaleValue: false,
+      displayValue: false,
+      displayMin: false,
+      displayMax: false,
+      prefix: '',
+      postfix: '',
 
-    // presenter
-    dataValues: [],
+      // presenter
+      dataValues: [],
 
-    // callbacks
-    onStart: () => {},
-    onChange: () => {},
-    onFinish: () => {},
-    onUpdate: () => {},
-  }, options);
+      // callbacks
+      onStart: () => {},
+      onChange: () => {},
+      onFinish: () => {},
+      onUpdate: () => {},
+    }, options);
 
-  this.each(function addPlugin(): void {
-    const $this = $(this);
-    const app = new SliderApp(settings, this);
+    this.each(function addPlugin(): void {
+      const $this = $(this);
+      const app = new SliderApp(settings, this);
 
-    $this.data('myMVPSlider', app);
-  });
+      $this.data('myMVPSlider', app);
+    });
 
-  return this;
-};
+    return this;
+  };
+}(jQuery));
