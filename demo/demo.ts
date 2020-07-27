5fe34fb738704ee02b1b0b33508dd6c9a4ce0d7e
kv-work <komaroff4@yandex.ru>
2020-07-27 22:10:48 +0500

    created Demo class

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,3 +1,26 @@
 import './demo.css';
 
 $('.js-slider').myMVPSlider();
+
+
+class Demo {
+  private $container: JQuery;
+  private slider: App;
+  private $configPanel: JQuery;
+
+  constructor($container: JQuery, options?: App.Option) {
+    this.$container = $container;
+
+    if (options) {
+      this.slider = this.$container.find('.js-slider').myMVPSlider(options).data('myMVPSlider');
+    } else {
+      this.slider = this.$container.find('.js-slider').myMVPSlider().data('myMVPSlider');
+    }
+
+    this.$configPanel = this.$container.find('.js-congig_panel');
+  }
+
+  private attachEventHandlers() {
+    
+  }
+}
